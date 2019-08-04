dbc444682dc69bd22cb2f9089151831572a22984
lazhcsilva <53198127+lazhcsilva@users.noreply.github.com>
2019-08-04 00:54:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function insereTexto()
+{document.getElementById('divTeste').innerHTML = 'Teste inserindo texto.';}
+
+$(document).ready(function() {
+
+
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+});
